e8491cdd58fc4058f2255acf3ba8c1bc2bab7cd9
domman95 <dominiklasek95@gmail.com>
2022-02-02 13:37:23 +0100

    fixed error: window is not available during ssr

--- site/src/components/layout.js
+++ site/src/components/layout.js
@@ -5,6 +5,9 @@ import Seo from './seo';
 import Footer from '../templates/footer';
 
 export default function Layout({ children }) {
+  const isBrowser = () => typeof window !== 'undefined';
+  isBrowser() && window.location.replace(res.data);
+
   return (
     <>
       <Seo />
